fcc7300cc21fd652d46f86930021153dd19de2b9
PatrykKonkiel <patryk.konkiel@gmail.com>
2026-09-08 16:23:06 +0200

    Merge pull request #6 from PatrykKonkiel/feature/PD-1
    PD-1: Stworzenie macierzy i choineczki

---
+++ src/main/java/pd1/FunkcjeMacierzy.java
@@ -0,0 +1,30 @@
+package pd1;
+
+public final class FunkcjeMacierzy {
+
+    private FunkcjeMacierzy() {
+
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static boolean isSymetric(int[][] matrix) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+
+}
